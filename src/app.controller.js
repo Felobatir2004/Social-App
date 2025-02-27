143b26d41904fc7da1d3de59bcc7d6cca311d6de
Felobatir2004 <felomories99@gmail.com>
2025-02-27 03:31:18 +0200

    a

--- src/app.controller.js
+++ src/app.controller.js
@@ -12,6 +12,7 @@ import {rateLimit} from "express-rate-limit"
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
     limit:2,
+    message:"Too many requests from this IP, please try again after 5 minutes"
 })
 const bootstrap = async (app, express)=>{
 
